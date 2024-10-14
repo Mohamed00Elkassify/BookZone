d2b2ae6ece74640a4c4ae0795efb125814247032
Mohamed00Elkassify <mohamed0elkassify@gmail.com>
2024-10-14 14:50:28 +0300

    Establish base file and layout for templates; define view and URL structure

--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.books_list, name='books_list'),
+    #path('', views.read_book, name='read_book'),
+    #path('', views.bookmark_book, name='bookmark_book'),
+    #path('', views.bookmarked_books, name='bookmarked_books'),
+    #path('', views.download_book, name='download_book'),
+    #path('', views.remove_bookmark, name='remove_bookmark'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Books
 # Create your views here.
+
+def books_list(request):
+    books = Books.objects.all()
+    return render(request, 'books/books_list.html', {'books' : books})
+
+def read_book(request):
+    pass
+
+def bookmark_book(Request):
+    pass
+
+def bookmarked_books(request):
+    pass
+
+def download_book(request):
+    pass
+
+def remove_bookmark(request):
+    pass
