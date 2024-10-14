400d9ec684cb55e223c12370771c7aa3baef667e
Mohamed00Elkassify <mohamed0elkassify@gmail.com>
2024-10-14 12:53:08 +0300

    Added Books model ; Apply migrations

--- BookZone/settings.py
+++ BookZone/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'books',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Books(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    pdf_link = models.URLField()
+    cover_image = models.ImageField(upload_to='cover/')
+    bookmarked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
