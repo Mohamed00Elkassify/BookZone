134bc777a6538926c89337eb1b449f1fa8c0c012
Mohamed00Elkassify <mohamed0elkassify@gmail.com>
2024-10-15 10:54:34 +0300

    Implement reading and bookmarking functionality with templates for reading and bookmarked books pages

--- books/urls.py
+++ books/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.books_list, name='books_list'),
-    #path('', views.read_book, name='read_book'),
-    #path('', views.bookmark_book, name='bookmark_book'),
-    #path('', views.bookmarked_books, name='bookmarked_books'),
+    path('read/<int:book_id>', views.read_book, name='read_book'),
+    path('bookmark/<int:book_id>', views.bookmark_book, name='bookmark_book'),
+    path('bookmarked/', views.bookmarked_books, name='bookmarked_books'),
+    path('remove/<int:book_id>', views.remove_bookmark, name='remove_bookmark'),
     #path('', views.download_book, name='download_book'),
-    #path('', views.remove_bookmark, name='remove_bookmark'),
 ]
--- books/views.py
+++ books/views.py
@@ -6,17 +6,25 @@ def books_list(request):
     books = Books.objects.all()
     return render(request, 'books/books_list.html', {'books' : books})
 
-def read_book(request):
-    pass
+def read_book(request, book_id):
+    book = get_object_or_404(Books, id=book_id)
+    return render(request, 'books/read_book.html', {'book' : book})
 
-def bookmark_book(Request):
-    pass
+def bookmark_book(Request, book_id):
+    book = get_object_or_404(Books, id=book_id)
+    book.bookmarked = True 
+    book.save()
+    return redirect('books_list')
 
 def bookmarked_books(request):
-    pass
+    bookmarked_books = Books.objects.filter(bookmarked=True)
+    return render(request, 'books/bookmarked_books.html', {'bookmarked_books' : bookmarked_books})
+
+def remove_bookmark(request, book_id):
+    book = get_object_or_404(Books, id=book_id)
+    book.bookmarked = False
+    book.save()
+    return redirect('bookmarked_books')
 
 def download_book(request):
-    pass
-
-def remove_bookmark(request):
     pass
